aec452b28da387ba70bbb414edce48e2662dfe08
Yusuf Güngör <yusufgngor@gmail.com>
2022-03-16 00:49:53 +0300

    Readme added

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Google OR-Tool VRP Case
+
+This repo is a case work. A Vehicle Route Problem is solved by using Google OR-Tool 
+
--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from Models.SolverModel import SolverModel
 from Models.input_model import InputModel
 from solver import solver
 
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Worasdasaaald"}
+    return {"Note": "To use Solver please send a post to /solver End point"}
 
 
 @app.post("/solver/")
 async def solve(inputModel: InputModel):
     model = SolverModel(inputModel)
     result = solver(model)
-
     return result
